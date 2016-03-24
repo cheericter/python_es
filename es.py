9c5cb433440d02800e18d520a351a54b6dea6396
sulixin <sulixinict@gmail.com>
2016-03-24 13:54:31 +0800

    s

--- es.py
+++ es.py
@@ -1,10 +1,29 @@
 from elasticsearch import Elasticsearch
 from elasticsearch import ConnectionError
 from elasticsearch import NotFoundError
+import codecs
+from functools import partial
+import sys
 
+def insert_id_score(infile):
+    es = Elasticsearch(["localhost:9200"])
+    with codecs.open(infile, 'r', 'utf-8') as infp:
+        for line in infp:
+            if not line.strip():
+                continue
+            row = line.strip().split('\t')
+            poiid = row[0]
+            raw_cscore = int10(row[1])
+            cscore = float(row[2])
+            #body = {}
+            score = {}
+            score['raw_cscore'] = raw_cscore
+            score['cscore'] = cscore
+            #body["doc"] = score
+            es.create(index=indexname, doc_type=typename, id=poiid, body=score)
 
 def updatefile():
-    id=1
+    poiid=1
     body ={}
     score = {}
     score['raw_cscore'] = 1
@@ -26,5 +45,11 @@ def updatefile():
     except Exception as e:
         print 'sth wrong'
         raise e
-updatefile()
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    indexname = "didi_poi_v1"
+    typename = "didi_score"
+    int10 = partial(int, base=10)
+    insert_id_score(infile)
+    #updatefile()
 
