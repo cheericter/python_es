a88fd5dff6bffe76011eaaea7eca9b65e19c1dda
sulixin <sulixinict@gmail.com>
2016-03-24 12:26:31 +0800

    d

--- es.py
+++ es.py
@@ -1,6 +1,24 @@
 from elasticsearch import Elasticsearch
 from elasticsearch import ConnectionError
 from elasticsearch import NotFoundError
+
+
+def updatefile():
+    id=1
+    body ={}
+    score = {}
+    score['raw_cscore'] = 1
+    score['cscore'] =1.000
+    body["doc"] = score
+    print body
+    try:
+        coll = Elasticsearch(['localhost:9200'])
+        msg = coll.update(index='megacorp', doc_type='employee', id=1,\
+                     body=body)
+        print msg
+        res = coll.search(index="megacorp", body={"query": {"match": {'last_name':'lixin'}}})
+        print res, type(res)
+
 def updatefile():
     try:
         coll=Elasticsearch(['localhost:9200'])
@@ -17,4 +35,6 @@ def updatefile():
     except Exception as e:
         print 'sth wrong'
         raise e
+
+updatefile()
 
